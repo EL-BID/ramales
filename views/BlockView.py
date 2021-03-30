4328967f8c1288ddaf8fd382dc1aac48c1a2cd7e
Fede Sanchez <federico.augusto.sanchez@gmail.com>
2021-03-29 21:30:43 -0300

    save nodes

--- views/BlockView.py
+++ views/BlockView.py
@@ -8,6 +8,7 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
         self.setupUi(self)
         self.proj = project
         self.current_block = None
+        self.current_nodes = None
         self.accepted.connect(self.save)
 
     def setData(self, block, nodes):
@@ -27,6 +28,7 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
             self.minSlope.setValue(block['minSlope'])
         self.observations.appendPlainText(self.check(block['comments']))        
         self.current_block = block
+        self.current_nodes = nodes
 
         nodesCount = len(nodes)
         self.tableWidget.setColumnCount(18)
@@ -65,21 +67,39 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
         self.show()
 
     def save(self):
-        layer = self.proj.getBlocksLayer()
+        #blocks
+        blocks = self.proj.getBlocksLayer()        
+        if not blocks.isEditable():
+                blocks.startEditing()
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('revision'), self.revision.text())
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('blockName'), self.blockName.text())
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('date'), self.date.date())
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('revDate'), self.revisionDate.date())
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('watershed'), self.watershed.text())
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('length'), self.totalLength.value())
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('minDepth'), self.minDepth.value())
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('minSlope'), self.minSlope.value())
+        blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('comments'), self.observations.toPlainText())
+        blocks.commitChanges()
         
-        if not layer.isEditable():
-                layer.startEditing()
+        #nodes
+        nodes = self.proj.getNodesLayer()
+        if not nodes.isEditable():
+            nodes.startEditing()
+        row = 0
+        for node in self.current_nodes:                        
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('id'), self.tableWidget.item(row,0).text() )
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('length'), self.tableWidget.item(row,1).text() )
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('username'), self.tableWidget.item(row,2).text() )
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('up_box'), self.tableWidget.item(row,3).text() )
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('down_box'), self.tableWidget.item(row,4).text() )
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('up_gl'), self.tableWidget.item(row,5).text() )
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('down_gl'), self.tableWidget.item(row,6).text() )
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('pvc_diam'), self.tableWidget.item(row,7).text() )
+            nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('comments'), self.tableWidget.item(row,17).text() )
+            row += 1
+        nodes.commitChanges()
         
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('revision'), self.revision.text())
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('blockName'), self.blockName.text())
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('date'), self.date.date())
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('revDate'), self.revisionDate.date())
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('watershed'), self.watershed.text())
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('length'), self.totalLength.value())
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('minDepth'), self.minDepth.value())
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('minSlope'), self.minSlope.value())
-        layer.changeAttributeValue( self.current_block.id(), layer.fields().lookupField('comments'), self.observations.toPlainText())
-        layer.commitChanges()
         self.proj.showMessage('saved successfully')
         self.hide()
 
