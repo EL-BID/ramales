47a5f3cdbf26baf4a8d5029ba749a14436c52772
martin <martindell.oro@gmail.com>
2021-04-02 13:32:43 -0300

    Adding new functions

--- views/BlockView.py
+++ views/BlockView.py
@@ -10,12 +10,12 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
         self.current_block = None
         self.current_nodes = None
         self.accepted.connect(self.save)
-        self.tableWidget.model().dataChanged.connect(self.onDataChanged)
+        self.tableWidget.itemChanged.connect(self.onItemChanged)
         self.minDepth.valueChanged.connect(self.depthMinChanged)
         self.minSlope.valueChanged.connect(self.slopeMinChanged)
 
     def setData(self, block, nodes):
-        #data mapper
+        self.tableWidget.blockSignals(True)
         self.revision.setText(self.check(block['revision']))
         self.blockName.setText(self.check(block['blockName']))
         if block['date']:
@@ -46,8 +46,11 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
             )
 
         # Set the table values
+        totalLength = 0
         for i in range(nodesCount):
-            node = nodes[i].attribute     
+            node = nodes[i].attribute
+            if node('length') != None :
+                totalLength = totalLength + float(node('length'))
             self.tableWidget.setItem(i, 0, QTableWidgetItem(self.check(node('up_box'))))
             self.tableWidget.setItem(i, 1, QTableWidgetItem(self.check(node('down_box'))))
             self.tableWidget.setItem(i, 2, QTableWidgetItem(self.check(node('length'))))
@@ -66,17 +69,18 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
             self.tableWidget.setItem(i, 15, QTableWidgetItem(self.check(node('comments'))))
             self.tableWidget.setItem(i, 16, QTableWidgetItem(self.check(node('username'))))
 
+        self.totalLength.setValue(float(totalLength))
         # Resize of the rows and columns based on the content
         self.tableWidget.resizeColumnsToContents()
         self.tableWidget.resizeRowsToContents()
         # Display the table
         self.tableWidget.show()
-
+        self.tableWidget.blockSignals(False)
         self.show()
 
     def save(self):
-        #blocks
-        blocks = self.proj.getBlocksLayer()        
+        self.tableWidget.blockSignals(True)
+        blocks = self.proj.getBlocksLayer()
         if not blocks.isEditable():
                 blocks.startEditing()
         blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('revision'), self.revision.text())
@@ -88,9 +92,6 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
         blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('minDepth'), self.minDepth.value())
         blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('minSlope'), self.minSlope.value())
         blocks.changeAttributeValue( self.current_block.id(), blocks.fields().lookupField('comments'), self.observations.toPlainText())
-        blocks.commitChanges()
-        
-        #nodes
         nodes = self.proj.getNodesLayer()
         if not nodes.isEditable():
             nodes.startEditing()
@@ -114,8 +115,9 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
             nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('comments'), self.tableWidget.item(row,15).text() )
             nodes.changeAttributeValue(node.id(), nodes.fields().lookupField('username'), self.tableWidget.item(row,16).text() )
             row += 1
+        blocks.commitChanges()
         nodes.commitChanges()
-        
+        self.tableWidget.blockSignals(False)
         self.proj.showMessage('saved successfully')
         self.hide()
 
@@ -123,53 +125,69 @@ class BlockViewDialog(QDialog, Ui_BlockDialog):
         return ('' if var == None else str(var))
 
     def depthMinChanged(self, val):
-        # print(val)
         self.calculate()
     
     def slopeMinChanged(self, val):
-        # print(val)
         self.calculate()
     
-    def onDataChanged(self, index, index2, roles):
-        val = index.data()
-        colName = self.tableWidget.horizontalHeaderItem(index.column()).text()
-        # colName = self.tableWidget.record(index.row()).fieldName(index.column())
-        # print(colName)
-        # if (colName == ''):
+    def onItemChanged(self, index):
+        if (index.column() == 12):
+            val = self.tableWidget.item(index.row(), index.column()).text()
+            self.tableWidget.blockSignals(True)
+            val = val if val != '' else None
+            self.tableWidget.setItem(index.row(), 7, QTableWidgetItem(val))
+            self.current_nodes[index.row()]['critDepth'] = val
+            self.tableWidget.blockSignals(False)
+            self.calculate()
 
     def calculate(self):
+        self.tableWidget.blockSignals(True)
         minDepth = self.minDepth.value()
         minSlope = self.minSlope.value()
         nodesCount = len(self.current_nodes)
         for i in range(nodesCount):
             node = self.current_nodes[i].attribute
-            length = node('length')
-            up_gl = node('up_gl')
-            down_box = node('down_box')
-            down_gl = node('down_gl')
-            #H21
-            upDepth = (minDepth) if down_box != 0 else ''
-            #F21
-            upBrLevel = (up_gl - upDepth) if down_box != 0 else ''
-            #G21
-            dwnBrLevel = "" if down_box == 0 else (0 if length == 0 else ((down_gl - minDepth) if (upBrLevel - (down_gl - minDepth))/length >= minSlope else round((upBrLevel - length * minSlope - 0.0005), 3)))
-            #I21
-            dwnDepth = (down_gl - dwnBrLevel) if down_box != 0 else ''
-            #J21 = 2 pero pueden modificar.
-            model = 2
-            #K21
-            upRuleLvl = "" if down_box == 0 else ("" if length == 0 else (model + upBrLevel ))
-            #L21 
-            dwnRuleLvl = "" if down_box == 0 else ("" if length == 0 else (model + dwnBrLevel))
-            #M21 critDepth puede modificarse y hace otro evento
-            # N21 slopeSection
-            slopeSection = "" if down_box == 0 else round((upBrLevel - dwnBrLevel)*100, 1)
+            nodePrev = self.current_nodes[i-1].attribute
+            length = float(node('length'))
+            up_gl = float(node('up_gl'))
+            down_box = float(node('down_box'))
+            down_gl = float(node('down_gl'))
+            critDepth = float(node('critDepth')) if node('critDepth') != None else 0
+            dwnDepthPrev = 0 if i == 0 else nodePrev('dwnDepth')
+            dwnBrLevelPrev = 0 if i == 0 else nodePrev('dwnBrLevel')
+            self.tableWidget.setRowCount(nodesCount)
+            if (i == 0):
+                upDepth = (minDepth) if down_box != 0 else ''  #H21 i==0
+                upBrLevel = (up_gl - upDepth) if down_box != 0 else '' #tocheck ''  #F21 i==0
+            else:
+                upBrLevel = 0 if down_box == 0 else (0 if length == 0 else (dwnBrLevelPrev if (dwnDepthPrev > critDepth) else (up_gl - critDepth)))
+                upDepth = (up_gl - upBrLevel) if (down_box != 0) else ""
+
+            dwnBrLevel = "" if down_box == 0 else (0 if length == 0 else ((down_gl - minDepth) if (upBrLevel - (down_gl - minDepth))/length >= minSlope else round((upBrLevel - length * minSlope - 0.0005), 3))) #G21
+            dwnDepth = (down_gl - dwnBrLevel) if down_box != 0 else ''#I21
+            model = 2 #J21 = 2 pero pueden modificar.
             
-            self.tableWidget.setItem(i, 5, QTableWidgetItem(str(upBrLevel)))
-            self.tableWidget.setItem(i, 6, QTableWidgetItem(str(dwnBrLevel)))
-            self.tableWidget.setItem(i, 7, QTableWidgetItem(str(upDepth)))
+            upRuleLvl = "" if down_box == 0 else ("" if length == 0 else (model + upBrLevel )) #K21
+            dwnRuleLvl = "" if down_box == 0 else ("" if length == 0 else (model + dwnBrLevel)) #L21 
+            #M21 critDepth puede modificarse y hace otro evento
+            slopeSection = "" if down_box == 0 else round((upBrLevel - dwnBrLevel) * 100, 1) # N21 slopeSection
+            
+            self.tableWidget.setItem(i, 5, QTableWidgetItem(str(round(upBrLevel, 3))))
+            self.current_nodes[i]['upBrLevel'] = upBrLevel
+            self.tableWidget.setItem(i, 6, QTableWidgetItem(str(round(dwnBrLevel, 3))))
+            self.current_nodes[i]['dwnBrLevel'] = dwnBrLevel
+            self.tableWidget.setItem(i, 7, QTableWidgetItem(str(round(upDepth, 2))))
+            self.current_nodes[i]['upDepth'] = upDepth
             self.tableWidget.setItem(i, 8, QTableWidgetItem(str(round(dwnDepth, 2))))
+            self.current_nodes[i]['dwnDepth'] = dwnDepth
             self.tableWidget.setItem(i, 9, QTableWidgetItem(str(model))) #tocheck
-            self.tableWidget.setItem(i, 10, QTableWidgetItem(str(upRuleLvl)))
-            self.tableWidget.setItem(i, 11, QTableWidgetItem(str(dwnRuleLvl)))
+            self.current_nodes[i]['model'] = model
+            self.tableWidget.setItem(i, 10, QTableWidgetItem(str(round(upRuleLvl, 3))))
+            self.current_nodes[i]['upRuleLvl'] = upRuleLvl
+            self.tableWidget.setItem(i, 11, QTableWidgetItem(str(round(dwnRuleLvl, 3))))
+            self.current_nodes[i]['dwnRuleLvl'] = dwnRuleLvl
+            self.tableWidget.setItem(i, 12, QTableWidgetItem(str(round(critDepth, 3))))
+            self.current_nodes[i]['critDepth'] = critDepth
             self.tableWidget.setItem(i, 13, QTableWidgetItem(str(slopeSection)))
+            self.current_nodes[i]['slopeSec'] = slopeSection
+        self.tableWidget.blockSignals(False)
